7a2affdb3218945e18e89ad073a85bf65532d084
pmvroque15 <pmvroque@gmail.com>
2026-05-12 09:39:29 -0400

    PLayed more around the inheritance practice

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -17,26 +17,31 @@ public class Main {
         car.setCargoCapacity(5);
         car.setColor("Midnight gray");
         car.setFuelCapacity(6);
+        car.setNumberOfPassengers(5);
 
         SemiTruck semiTruck = new SemiTruck();
 
         semiTruck.setColor("White");
         semiTruck.setCargoCapacity(4);
         semiTruck.setFuelCapacity(8);
+        semiTruck.setNumberOfPassengers(7);
+
 
         Hovercraft hovercraft = new Hovercraft();
 
         hovercraft.setColor("Black");
         hovercraft.setCargoCapacity(10);
         hovercraft.setFuelCapacity(20);
+        hovercraft.setNumberOfPassengers(20);
 
         vehicles.add(moped);
-        vehicles.size();
+        vehicles.add(semiTruck);
+        vehicles.add(hovercraft);
+        vehicles.add(car);
 
-        int number = 0;
+
         for (Vehicle v : vehicles) {
-            number++;
-            System.out.printf("VEHICLE %s %nCOLOR: %s | CARGO CAPACITY: %s | FUEL CAPACITY: %s %n", number, v.getColor(), v.getCargoCapacity(), v.getFuelCapacity());
+            System.out.printf("%nVEHICLE %s %nCOLOR: %s | CARGO CAPACITY: %s | FUEL CAPACITY: %s | PASSENGER CAPACITY: %s %n", vehicles.size(), v.getColor(), v.getCargoCapacity(), v.getFuelCapacity(), v.getNumberOfPassengers());
         }
 
         if (vehicles.isEmpty()) {
